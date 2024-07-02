3cdc4e53ad298d62038f6248ba380484332c4fe5
Yisamoses1 <yisarasaq2018@gmail.com>
2024-07-02 08:54:43 +0100

    Updated version

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+require("dotenv").config()
 const express = require("express");
-const ip = require("ip");
 const axios = require("axios");
 const cors = require("cors");
 
@@ -8,31 +8,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const OPENWEATHERMAP_API_KEY = "73c5abf033e0d9c90c11cfbd5fb3b94c"; 
+const OPENWEATHERMAP_API_KEY =
+  process.env.OPENWEATHERMAP_API_KEY ; 
 
 app.get("/api/hello", async (req, res) => {
   const visitorName = req.query.visitor_name || "Visitor";
 
-  //Visitor's IP address
-  const visitorIp = req.ip || ip.address();
+  // Attempt to get the client's IP address, considering Vercel's proxy setup
+  const visitorIp =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  
 
   try {
     // Fetch the location of the visitor using ipwhois.app
-    const locationResponse = await axios.get(`https://ipwhois.app/json/${visitorIp}`);
-    
+    const locationResponse = await axios.get(
+      `https://ipwhois.app/json/${visitorIp}`
+    );
 
-    // Extrat city and country
+
+    // Extract city and country
     const { city, country } = locationResponse.data;
 
+    if (!city || !country) {
+      throw new Error("City or country not found in location response");
+    }
+
     // Fetch weather using OpenWeatherMap
-    const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`);
-  
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`
+    );
+   
 
     // Get temperature information
     const temperature = weatherResponse.data.main.temp;
 
     // Construct greetings message using the given format
-    const greetings = `Hello, ${visitorName}!, the temperature is ${temperature}°C in ${city}, ${country}.`;
+    const greetings = `Hello, ${visitorName}! The temperature is ${temperature}°C in ${city}, ${country}.`;
 
     const responseData = {
       client_ip: visitorIp,
@@ -40,22 +51,13 @@ app.get("/api/hello", async (req, res) => {
       greetings: greetings,
     };
 
-
     res.json(responseData);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch data" });
+    res.status(500).json({ error: error.message });
   }
 });
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
-
-
-
-
